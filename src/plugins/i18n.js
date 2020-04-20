997c76dd5a77e5cbc58a8e1a8619e4fe097303a2
Tony DUONG <astowny@gmail.com>
2020-04-20 14:19:51 +0200

    translation ok

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,25 +1,20 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
+import enLang from '../lang/en.json'
+import frLang from '../lang/fr.json'
+import jaLang from '../lang/ja.json'
 
 Vue.use(VueI18n)
 
 
 const messages = {
-  en: {
-    title: "Supeeeeer Heroes !!!",
-    details: "{name} Details"
-  },
-  ja: {
-    title: 'スーパーヒーロー !!!',
-    details: "{name}の詳細"
-  },
-  fr: {
-    title: "Supeeeeer Héros !!!",
-    details: "Détails de {name}"
-  }
+  en: enLang,
+  fr: frLang,
+  ja: jaLang,
 }
 
 export const i18n = new VueI18n({
   locale: 'en', // set locale
   messages, // set locale messages
 })
+
