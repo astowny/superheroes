c5b66613daa3ee69bb45ab1410c02c184ac20256
Tony DUONG <astowny@gmail.com>
2020-04-21 16:37:43 +0200

    added btn more heroes

--- notes.md
+++ notes.md
@@ -1,7 +1,14 @@
-doing validation
 
-add translation to error msg
+[08:08:15] Jacques Corbin: http://vuex.vuejs.org/
+
+[08:08:28] Jacques Corbin: https://kazupon.github.io/vue-i18n/
+
+[08:08:42] Quentin: https://vuetifyjs.com/en/
+
+
 getmore heroes doesnt work in heroes
 
 error on addhero when updating imgurl
 NaN when entering string with number on addhero
+
+the style or position of the sort btn is not very pretty
--- src/components/Heroes/_subs/Pagination.vue
+++ src/components/Heroes/_subs/Pagination.vue
@@ -1,72 +1,78 @@
 <template>
-  <v-pagination
-    v-model="currentPage"
-    :length="pageCount"
-    circle
-    @input="emitInput()"
-    :total-visible="totalVisible"
-  ></v-pagination>
+	<div class="d-flex justify-center align-center px-4">
+		<v-pagination
+			v-model="currentPage"
+			:length="pageCount"
+			@input="emitInput()"
+			:total-visible="totalVisible"
+			circle
+		>
+    </v-pagination>
+		<v-btn @click="moreHeroes()" class="primary">
+      {{$t('paginationmore')}}
+    </v-btn>
+	</div>
 </template>
 
 <script>
+import { mapState, mapActions } from "vuex";
+import { GET_MORE_HEROES } from "../../../store/types/actions-types";
 /**
  * Set the array of heroes for the current page
  */
 
 export default {
-  props: {
-    heroes: {
-      type: Array,
-      required: true
-    }
-  },
-  name: "Pagination",
-  data() {
-    return {
-      totalVisible: 7
-    };
-  },
-  methods: {
-    emitInput() {
-      this.$emit("input", this.currentPageHeroes);
-    }
-  },
-  watch: {
-    /**
-     * Loads more heroes when the current page is before the last
-     */
-    currentPage() {
-      let offset = this.heroes.length;
-      if (this.currentPage >= this.pageCount - 1) {
-        this.$store.dispatch("getMoreHeroes", offset);
-      }
-    }
-  },
-  computed: {
-    visibleItemsPerPageCount() {
-      return this.$store.state.pagination.visibleItemsPerPageCount;
-    },
-    // number of pages in pagination
-    pageCount() {
-      let euclidianDiv = Math.trunc(
-        this.heroes.length / this.visibleItemsPerPageCount
-      );
-      let rest = this.heroes.length % this.visibleItemsPerPageCount;
-      if (rest === 0) {
-        return euclidianDiv;
-      } else {
-        return euclidianDiv + 1;
-      }
-    },
-    currentPage: {
-      get() {
-        return this.$store.state.pagination.currentPage;
-      },
-      set(value) {
-        this.$store.commit("setCurrentPage", value);
-      }
-    }
-  }
+	props: {
+		heroes: {
+			type: Array,
+			required: true
+		}
+	},
+	name: "Pagination",
+	data() {
+		return {
+			totalVisible: 5
+		};
+	},
+	methods: {
+		emitInput() {
+			this.$emit("input", this.currentPageHeroes);
+		},
+		/**
+		 * Loads more heroes
+		 */
+		moreHeroes() {
+			let offset = this.heroes.length;
+			this.$store.dispatch(GET_MORE_HEROES, offset);
+		}
+	},
+	computed: {
+		...mapState({
+			visibleItemsPerPageCount: state =>
+				state.pagination.visibleItemsPerPageCount
+		}),
+		...mapActions([GET_MORE_HEROES]),
+		// number of pages in pagination
+		pageCount() {
+			let euclidianDiv = Math.trunc(
+				this.heroes.length / this.visibleItemsPerPageCount
+			);
+			let rest = this.heroes.length % this.visibleItemsPerPageCount;
+			if (rest === 0) {
+				return euclidianDiv;
+			} else {
+				return euclidianDiv + 1;
+			}
+		},
+		currentPage: {
+			get() {
+				return this.$store.state.pagination.currentPage;
+			},
+			set(value) {
+				this.$store.commit("setCurrentPage", value);
+			}
+		}
+	}
 };
 </script>
 
