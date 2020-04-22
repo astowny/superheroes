f46cb78d278290db0ceb9c911849afc259127d11
Tony DUONG <astowny@gmail.com>
2020-04-22 16:11:46 +0200

    fix selected hero no longer used

--- src/components/Heroes/index.vue
+++ src/components/Heroes/index.vue
@@ -11,7 +11,6 @@
 			<!-- list items -->
 			<v-list-item
 				v-for="(hero) in currentPageHeroes"
-				:class="{secondary : hero === selectedHero}"
 				:key="hero.name"
 				@click="selectHero(hero)"
 			>
@@ -19,16 +18,13 @@
 					<v-img :src="hero.imgUrl"></v-img>
 				</v-list-item-avatar>
 
-				<v-list-item-content :class="hero === selectedHero ? 'white--text' :''">
+				<v-list-item-content>
 					<v-list-item-title v-html="`${hero.id} - ${hero.name}`"></v-list-item-title>
-					<v-list-item-subtitle
-						v-html="hero.description"
-						:class="hero === selectedHero ? 'white--text' :''"
-					></v-list-item-subtitle>
+					<v-list-item-subtitle v-html="hero.description"></v-list-item-subtitle>
 				</v-list-item-content>
 			</v-list-item>
 			<!--  pagination  -->
-			<pagination :heroes="heroes"/>
+			<pagination :heroes="heroes" />
 			<!-- btn moreheroes -->
 		</div>
 		<!-- end content -->
@@ -38,7 +34,10 @@
 <script>
 import Pagination from "./_subs/Pagination";
 import { mapState, mapMutations } from "vuex";
-import { SORT_HEROES_BY_NAME, SET_SELECTED_HERO } from "../../store/types/mutations-types";
+import {
+	SORT_HEROES_BY_NAME,
+	SET_SELECTED_HERO
+} from "../../store/types/mutations-types";
 
 export default {
 	name: "Heroes",
@@ -47,7 +46,7 @@ export default {
 	},
 	methods: {
 		selectHero(hero) {
-				this.$store.commit(SET_SELECTED_HERO, hero.id)
+			this.$store.commit(SET_SELECTED_HERO, hero.id);
 		}
 	},
 	computed: {
